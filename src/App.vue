49c4ef953b05ab914d3b0a48069020e8a566df0c
hejinxue001 <hejinxue001@163.com>
2018-08-20 12:55:21 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,54 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <el-container>
+        <el-header>Header</el-header>
+        <el-container>
+            <el-aside width="200px">Aside</el-aside>
+            <el-main>
+                <div>
+                  <router-view/>                  
+                </div>
+            </el-main>
+        </el-container>
+    </el-container>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<style>
+  .el-header, .el-footer {
+    background-color: #B3C0D1;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
   }
-}
+  
+  .el-aside {
+    background-color: #D3DCE6;
+    color: #333;
+    float: left;
+    text-align: center;
+    line-height: 200px;
+  }
+  
+  .el-main {
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+    line-height: 160px;
+    margin-left: 220px;
+
+  }
+  
+  body > .el-container {
+    margin-bottom: 40px;
+  }
+  
+  .el-container:nth-child(5) .el-aside,
+  .el-container:nth-child(6) .el-aside {
+    line-height: 260px;
+  }
+  
+  .el-container:nth-child(7) .el-aside {
+    line-height: 320px;
+  }
 </style>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,15 @@
+<template>
+    <ul>
+        <li>
+            <span>平台</span>
+           <router-link to="/home">Home</router-link>
+        </li>
+    </ul>
+</template>
+
+<script>
+export default {
+    name:'SideBar'
+}
+</script>
+
